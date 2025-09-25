2193bd817012e58f87be8bb639100de11625bbfe
Hamzeh Zaza <hamoz.zaza@outlook.com>
2025-09-25 23:21:31 +0200

    A Morphology Function was added but still needs some work

--- .venv/Image Preprocessing/Image_Processing.py
+++ .venv/Image Preprocessing/Image_Processing.py
@@ -3,28 +3,35 @@ import PIL as pl
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread("../Images/Untitled.png")
-img2 = cv.imread("../Images/images.jpeg")
-assert img is not None, "File does not contain an image or file doesn't exist"
+src_img = cv.imread("../Images/Untitled.png")
+assert src_img is not None, "File does not contain an image or file doesn't exist"
 
-img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+gray_img = cv.cvtColor(src_img, cv.COLOR_BGR2GRAY)
 
-img = cv.GaussianBlur(img, [5, 5], 0)
+blur_img = cv.GaussianBlur(gray_img, [3, 3], 0)
 
-Lap = cv.Laplacian(img, cv.CV_8U, 1, 3)
+Lap = cv.Laplacian(blur_img, cv.CV_8U, ksize=1)
 
-ret, res = cv.threshold(Lap, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+ret, img_thrs = cv.threshold(Lap, 1, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
 
-cont, hierarchy = cv.findContours(Lap, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+# plt.imshow(img_thrs)
+# plt.show()
 
-res = cv.cvtColor(res,cv.COLOR_GRAY2BGR)
+cont, hierarchy = cv.findContours(img_thrs, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+res = cv.cvtColor(img_thrs,cv.COLOR_GRAY2BGR)
 
 cv.drawContours(res, cont, (-1), (0,255,0), 1)
 
-res = cv.resize(res, dsize=(900,500))
-# res1 = cv.resize(img2, None, fx=1.2, fy=1.2,interpolation=cv.INTER_AREA)
+# plt.imshow(res)
+# plt.show()
 
-plt.imshow(res)
+kernal = np.ones((2, 2), np.uint8)
+cls_img = cv.morphologyEx(res, cv.MORPH_CLOSE, kernal)
+
+cls_img = cv.resize(cls_img, dsize=(900,500))
+
+plt.imshow(cls_img)
 plt.show()
 
 cv.waitKey(0)
